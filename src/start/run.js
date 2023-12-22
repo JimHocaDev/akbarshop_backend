4449ce623402770b59137b8b6b8506fa51b1d096
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2023-12-22 19:26:23 +0500

    commit

--- src/start/run.js
+++ src/start/run.js
@@ -3,7 +3,9 @@ const config = require("config");
 const Admins = require("../models/Admin");
 
 const run = async (app) => {
-  await connect(config.get("MONGO_URI"));
+  const MONGO_URI = config.get("MONGO_URI");
+
+  await connect(MONGO_URI);
 
   const admin = await Admins.findOne();
   const username = config.get("admin_username");
