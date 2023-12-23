d9c7e6b63c470a1cf13917233c2525af645130f9
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2023-12-23 11:29:18 +0500

    commit

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = process.env.SECRET_KEY;
+const SECRET_KEY = process.env.SECRET_KEY || "this323jasdIASF22#$qaskey";
 
 const sign = (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
 
