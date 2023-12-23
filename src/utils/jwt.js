726b022c8811c26757178139451b6776ef84bc1b
ZiyodillaDev <ziyodillazokirkhujayev@gmail.com>
2023-12-23 11:19:27 +0500

    commit

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,10 +1,10 @@
 const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.SECRET_KEY;
 
-const sign = (payload) =>
-  jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: "24h" });
+const sign = (payload) => jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
 
 const verify = (payload, err, data) => {
-  jwt.verify(payload, process.env.SECRET_KEY, err, data);
+  jwt.verify(payload, SECRET_KEY, err, data);
 };
 
 module.exports = {
